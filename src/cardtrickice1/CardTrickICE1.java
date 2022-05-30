7e03ac964eaf323950a1a91503528060d484e473
3RamanKaur <kau11821@sheridancollege.ca>
2022-05-30 17:05:09 -0400

    version 1 ICE

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -4,6 +4,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//Ramandeep Kaur (Student id-991661772)
 package cardtrickice1;
 
 import java.util.Scanner;
