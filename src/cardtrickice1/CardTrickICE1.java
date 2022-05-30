79b41e8b664646f54cc2259b08c20a43e9ab9d8e
3Ramankaur <105507287+3Ramankaur@users.noreply.github.com>
2022-05-30 16:52:55 -0400

    Update CardTrickICE1.java

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -20,9 +20,13 @@ public class CardTrickICE1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) 
-    {
-        Card[] magicHand = new Card[7]; //Array of object
-        for( int i=0;i<magicHand.length;i++)
+    {   Card luckyCard= new Card();
+        luckyCard.setValue(5);
+        luckyCard.setSuits("clubs");
+        
+        Card[] magicHand = new Card[8]; //Array of object
+        magicHand[0]=luckyCard;
+        for( int i=1;i<magicHand.length;i++)
         {
             Card c1 = new Card();
             c1.setValue(c1.number());//use a method to generate random *13
